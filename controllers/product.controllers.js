3ed1be8b44445090e55b4a6946b1eebc75ffb200
dulcegomeza <athaladulceedithgomezarellano@gmail.com>
2022-11-22 15:59:48 -0600

    prueba paginado 1

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -3,12 +3,18 @@ const { Product } = require('../models');
 
 const productsPaginadoPost = async(req, res) =>{
 
-    const {category = '', desde = 0, limite = 9 } = req.body;
+    const {category = '', desde = 0, limite = 9, page =1 } = req.body;
 
     let query = { status: true};
-
+    let page_next = 0;
+    let page_afther  = 0;
+    
     if(category!=''){
         query = { status: true, category: category};
+    }
+
+    if(desde < 0){
+        desde = 0;
     }
    
 
@@ -19,9 +25,31 @@ const productsPaginadoPost = async(req, res) =>{
         Product.countDocuments(query)
     ])
 
-    const pages = total / limite;
+    const total_pages = Math.ceil(total / limite);
 
-    res.json({ products, total, pages })
+    if(page > total_pages){
+        page = total_pages;
+    }
+
+    if(page >= total_pages -1){
+        page_next = 1;
+    }else{
+        page_next = page + 2;
+    }
+
+    if(page <1){
+        page_afther = total_pages;
+    }else{
+        page_afther = page;
+    }
+
+
+    const page_actual = page +1;
+
+
+   
+
+    res.json({ products, total, total_pages, page_actual, page_next, page_afther })
 }
 
 const productsGet = async(req, res) =>{
