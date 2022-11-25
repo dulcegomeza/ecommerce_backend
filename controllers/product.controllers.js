c80cbb64d9746dfc05aba2b8fc2b2024d169fc66
dulcegomeza <athaladulceedithgomezarellano@gmail.com>
2022-11-24 18:11:00 -0600

    p4

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -35,6 +35,7 @@ const productsPaginadoPost = async (req, res) => {
     }
 
     const products = await Product.find(query)
+        .populate('category', 'name')
         .skip(Number(desd)).limit(Number(limite));
 
     const page_actual = page + 1;
